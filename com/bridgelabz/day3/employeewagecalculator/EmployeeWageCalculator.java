ad59589ffd5a79c5880f54fa988a31675f5766da
codeaadi <adityadubey362@gmail.com>
2023-01-08 18:28:27 +0530

    Added: Welcome messege for Employee Wage Computation Program

---
+++ com/bridgelabz/day3/employeewagecalculator/EmployeeWageCalculator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day3.employeewagecalculator;
+
+public class EmployeeWageCalculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Calculation Program");
+    }
+}
